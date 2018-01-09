99ba9ebb6ed0ce0590efc8b9e45b947bdcf65a17
anthonyrotiroti <anthony.rotiroti@gmail.com>
2018-01-09 12:57:39 -0500

    Add files via upload

--- src/content.js
+++ src/content.js
@@ -1,39 +1,33 @@
+// Global variables:
+
+// An array of professors read from the Spire page.
 var professors = [];
 
-
+// A count of how many professor rating async calls have been completed.
 var profsLoaded = 0;
 
-var observer = new MutationObserver(function(){console.log("mutation");});
-observer.observe(document, {childList: true, subtree: true});
+// The iframe of the page, which contains the content.
+var iframe = document.getElementById('ptifrmtgtframe');
 
-iframe = document.getElementById('ptifrmtgtframe');
 
-function runScript(){
-	console.log("runScript called");
-	//var iframe = document.getElementById('ptifrmtgtframe');
-	var el = iframe.contentDocument.getElementById('MTG_INSTR$0');
-	//if(!el){
-		// A lower timeout causes different-or0igin error. Maybe because of more frequent runScript calls.
-		//window.setTimeout(runScript,1000); 
-		//console.log("no profs");
-		//return;
-	//}
+function getProfs(){
+   
+	var element;
+	for(var i = 0; iframe.contentDocument.getElementById('MTG_INSTR$'+i); i++){
+		element = iframe.contentDocument.getElementById('MTG_INSTR$'+i);
 
-	var i = 0;
-	while(el){
-		if(el.innerHTML !== 'Staff' && el.innerHTML !== 'TBA')
-			professors.push(el);
-		i++;
-		el = iframe.contentDocument.getElementById('MTG_INSTR$'+i);
-	}
+		if(element.innerHTML !== 'Staff' && element.innerHTML !== 'TBA'){
+			var names = element.innerHTML.split(/,\s<br>/);
 
-	professors.forEach(function(prof){
-		prof.name = prof.innerHTML;
-		prof.urlName = encodeName(prof.name);
-		prof.rating = -1;
-
-		findProfRating(prof,findProfRatingCallback);
-	});
+			for(var j = 0; j < names.length; j++){
+				var prof = element;
+				prof.name = names[j];
+				prof.rating = -1;
+				professors.push(prof);
+				findProfRating(prof, j, names.length-1,  prof.name, findProfRatingCallback);
+			}
+		}
+	}	
 }
 
 
@@ -44,32 +38,33 @@ function encodeName(name){
 }
 
 
-function findProfRating(prof, callback){
+function findProfRating(prof, index, lastIndex, name, callback){
+	prof.innerHTML = '';
 
-	var url = 'https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=University+of+Massachusetts&schoolID=1513&query=' + prof.urlName;
-	//console.log(url);
+	var url = 'https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=University+of+Massachusetts&schoolID=1513&query='
+	+ encodeName(name);
+
 	var xhr = new XMLHttpRequest();
 
 	xhr.open("GET", url , true); 
 	xhr.onload = function(){
-		//if(xhr.readyState == 4 && xhr.status == 200)
-			
-			// Parsing html of search page and extracting link to prof
+
 		var parser = new DOMParser();
 		var doc = parser.parseFromString(xhr.responseText,'text/html');
 		var result = doc.querySelector(".listing.PROFESSOR")
 
 		if(result)
-			callback(prof, result.firstElementChild.getAttribute('href'));
-		else
-			prof.insertAdjacentHTML('beforeend', '  Not Found');
+			callback(prof, index, lastIndex, name, result.firstElementChild.getAttribute('href'));
 
+		else
+			prof.innerHTML += (name + '  Not Found');
 	}
+
 	xhr.send(null);
 }
 
 
-function findProfRatingCallback(prof, page){
+function findProfRatingCallback(prof, index, lastIndex, name, page){
 	var url = "https://www.ratemyprofessors.com" + page;	
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url, true);
@@ -79,35 +74,31 @@ function findProfRatingCallback(prof, page){
 		var doc = parser.parseFromString(xhr.responseText, 'text/html');
 		var rating = doc.querySelector("div.breakdown-container.quality").children[0].children[0].innerHTML;
 
-		prof.insertAdjacentHTML('beforeend', '  <a href="'+url+'" target =_blank>'+rating+'</a>');
+		prof.innerHTML += (name + '  <a href="' + url + '" target =_blank>' + rating + '</a> <br>');
 		profsLoaded++;
 
 		if(profsLoaded == professors.length)
-			wait();
-		
+			wait();	
 	}
+	
 	xhr.send(null);
 }
 
 
-//checkIFrame();
-
-
 function wait(){
-	console.log("wait");
 	setTimeout(function(){
 		var el = iframe.contentDocument.getElementById('MTG_INSTR$0');	
-		if(professors.length > 0 && !el){
+		if(professors.length > 0 && !el)
 			reset();
-		}	
+			
 		if(el && (professors.length == 0)){
-			runScript();
+			getProfs();
 			wait();
 		}
 		
-		else{
+		else
 			wait();
-		}
+		
 	},1000);
 
 	}
@@ -117,14 +108,6 @@ function reset(){
 	professors = [];
 	profsLoaded = 0;
 }
-	//setTimeout(1000, function(){
-		//On prof page -> insert ratings
-		//if(iframe.contentDocument.getElementById('MTG_INSTR$0')){ //&& (professors.length == 0 || profsLoaded < professors.length)){
-			//console.log("hit");
-			//observer.disconnect(); //Don't want to trigger observer with rating insertion
-			//runScript();
-		//}
-	//};
 
 
 wait();
